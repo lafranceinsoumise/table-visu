8f0bb4d7202b03cb7e07c277b71284921803df71
Jill Royer <perso@guilro.com>
2018-12-08 00:19:51 +0100

    récupère allowed_hosts dans l'env

--- back/table_visu/settings.py
+++ back/table_visu/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(",")
 
 
 # Application definition
