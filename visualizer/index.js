7522ab85125b24f97ac24d1b2632c7c15854ba1d
Arthur Cheysson <arthur@cheysson.fr>
2018-12-08 12:04:32 +0100

    Finalisation visualize

--- visualizer/index.js
+++ visualizer/index.js
@@ -36,14 +36,16 @@ const numberScale = d3.scaleSequential(interpolateRdYlGn).domain([0, 1])
 const agreementScale = d3.scaleOrdinal(agreementColors).domain([0, 1])
 const grey = '#888'
 
-function Table (g, tableCoords, data) {
+function Table (g, tableCoords) {
 
   const circleGroup = g.append('g')
   const textGroup = g.append('g')
   const sceneGroup = g.append('g')
 
   return function refresh (data) {
-    const dimensions = [0.8 * window.innerWidth - margin.left - margin.right, window.innerHeight - margin.top - margin.bottom]
+    const svgDimensions = g.node().parentNode.getBoundingClientRect()
+
+    const dimensions = [svgDimensions.width - margin.left - margin.right, svgDimensions.height - margin.top - margin.bottom]
     const joinedData = tableCoords.map(d => Object.assign({ value: data[d.table] }, d))
 
     const circles = circleGroup
@@ -104,44 +106,78 @@ function Table (g, tableCoords, data) {
   }
 }
 
-function Legend (div) {
-  const upper = div.append('div')
-    .text('Pas d\'accord')
-    .style('text-align', 'left')
+function legendNumber (div) {
 
-  const canvas = div.append('canvas')
+  div.attr('class', 'legendNumber')
+  const left = div.append('div').attr('class', 'left')
+  const right = div.append('div').attr('class', 'right')
+    .style('display', 'flex').style('flex-direction', 'column').style('justify-content', 'space-between')
+
+  const upper = right.append('div')
+    .text('D\'accord')
+
+  const lower = right.append('div')
+    .text('Pas d\'accord')
+
+  const canvas = left.append('canvas')
   const ctx = canvas.node().getContext('2d')
 
-  const lower = div.append('div')
-    .text('D\'accord')
-    .style('text-align', 'right')
+  const height = 160
 
-  return function refresh () {
-    const width = 0.2 * window.innerWidth - 40
+  canvas.attr('height', height)
+    .attr('width', 1)
+    .style('height', height + 'px')
+    .style('width', '30px')
+    .style('border', '1px solid black')
 
-    canvas.attr('height', 1)
-      .attr('width', width)
-      .style('height', '40px')
-      .style('width', width + 'px')
-      .style('border', '1px solid black')
+  ctx.clearRect(0, 0, 1, height)
 
-    ctx.clearRect(0, 0, width, 1)
+  const legendScale = d3.scaleLinear()
+    .range([0, height])
+    .domain(numberScale.domain())
 
-    const legendScale = d3.scaleLinear()
-      .range([0, width])
-      .domain(numberScale.domain())
+  const image = ctx.createImageData(1, height)
 
-    const image = ctx.createImageData(width, 1)
+  d3.range(height).forEach(function (i) {
+    const c = d3.rgb(numberScale(legendScale.invert(height - i)))
+    image.data[4 * i] = c.r
+    image.data[4 * i + 1] = c.g
+    image.data[4 * i + 2] = c.b
+    image.data[4 * i + 3] = 255
+  })
 
-    d3.range(width).forEach(function (i) {
-      var c = d3.rgb(numberScale(legendScale.invert(i)))
-      image.data[4 * i] = c.r
-      image.data[4 * i + 1] = c.g
-      image.data[4 * i + 2] = c.b
-      image.data[4 * i + 3] = 255
-    })
+  ctx.putImageData(image, 0, 0)
+}
 
-    ctx.putImageData(image, 0, 0)
+function legendBoolean (div) {
+  const consensus = div.append('div')
+  const desaccord = div.append('div')
+
+  desaccord.append('div').attr('class', 'case').style('background-color', agreementColors[0])
+  consensus.append('div').attr('class', 'case').style('background-color', agreementColors[1])
+
+  desaccord.append('span').text('Pas de consensus')
+  consensus.append('span').text('Consensus atteint')
+}
+
+function Legend (div) {
+  let current = null
+
+  return function refresh (data) {
+    if (data.type !== current) {
+      current = data.type
+
+      if (div.node().firstChild) {
+        div.node().firstChild.remove()
+      }
+
+      if (current === 'number') {
+        legendNumber(div.append('div'))
+      } else {
+        legendBoolean(div.append('div'))
+      }
+
+    }
   }
 }
 
@@ -174,7 +210,7 @@ function Groups (div) {
   }
 }
 
-function setUp (tableGroup, legendGroup, groupGroup) {
+function setUp (tableGroup, legendGroup) {
   const table = Table(tableGroup, tableCoords)
   const legend = Legend(legendGroup)
   //const group = Groups(groupGroup)
@@ -188,7 +224,7 @@ function setUp (tableGroup, legendGroup, groupGroup) {
 
   function refresh (data) {
     table(data)
-    legend()
+    legend(data)
     //group(data)
   }
 
@@ -205,12 +241,14 @@ function setUp (tableGroup, legendGroup, groupGroup) {
 }
 
 function main () {
-  const svg = d3.select('body').append('svg').attr('class', 'main').attr('shape-rendering', 'geometricPrecision')
-  const div = d3.select('body').append('div').attr('class', 'sidebar')
+  const content = d3.select('body').append('div').attr('class', 'content')
+  const svg = content.append('svg').attr('class', 'main').attr('shape-rendering', 'geometricPrecision')
+  const div = content.append('div').attr('class', 'sidebar')
   const tables = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
   const legendGroup = div.append('div')
-  const groupGroup = div.append('div')
-  setUp(tables, legendGroup, groupGroup)
+  setUp(tables, legendGroup)
+
+  const link = d3.select('body').append('div').attr('class', 'link').text('la-fi.fr/convention2018')
 }
 
 main()
